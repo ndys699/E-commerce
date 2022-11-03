1d2403a4871a1f58b555cd2befea9dd206164345
sandeep <ndys699@gmail.com>
2022-11-03 17:23:38 +0530

    added

---
+++ webclient/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Helow from './Components/Helow';
+
+function App() {
+  return (
+    <div className="App">
+      <Helow></Helow>
+    </div>
+  );
+}
+
+export default App;
---
+++ webclient/src/Components/Helow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Helow() {
+  return (
+    <div>Helow</div>
+  )
+}
+
+export default Helow
